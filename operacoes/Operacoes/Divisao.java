baa35e53cfbc84f8af20d00fdc5d05782492208e
robertoantonioayres <61851682+robertoantonioayres@users.noreply.github.com>
2020-03-11 18:20:03 -0300

    Iniciando projeto calculadora

---
+++ operacoes/Operacoes/Divisao.java
@@ -0,0 +1,15 @@
+package Operacoes;
+
+import javax.swing.JOptionPane;
+
+public class Divisao {
+	public static void main(String[] args) {
+		float a;
+		float b;
+		float res;
+		a = Float.parseFloat(JOptionPane.showInputDialog("Digite o primeiro numero:"));
+		b = Float.parseFloat(JOptionPane.showInputDialog("Digite o segundo numero:"));
+		res = a / b;
+		JOptionPane.showMessageDialog(null,"O resultado é: " + res);
+	}
+}
